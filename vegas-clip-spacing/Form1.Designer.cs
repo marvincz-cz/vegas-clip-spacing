73ecb619edf65cb010235d879515cd282e038266
Petr Fuka <petr.fuka@gmail.com>
2021-11-15 18:17:32 +0100

    Dialog styling, icon

--- vegas-clip-spacing/Form1.Designer.cs
+++ vegas-clip-spacing/Form1.Designer.cs
@@ -29,31 +29,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.DecimalPlaces = 1;
-            this.numericUpDown1.Location = new System.Drawing.Point(21, 22);
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(56, 72);
             this.numericUpDown1.Maximum = new decimal(new int[] {600, 0, 0, 0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(144, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 28);
             this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.numericUpDown1.Value = new decimal(new int[] {8, 0, 0, 0});
             this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 54);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.button1.Location = new System.Drawing.Point(12, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 34);
+            this.button1.Size = new System.Drawing.Size(234, 34);
             this.button1.TabIndex = 1;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.title.Location = new System.Drawing.Point(12, 19);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(234, 31);
+            this.title.TabIndex = 3;
+            this.title.Text = "Pause length";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.label1.Location = new System.Drawing.Point(133, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "seconds";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
@@ -61,14 +87,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(185, 126);
+            this.ClientSize = new System.Drawing.Size(258, 180);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Spacing";
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label title;
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.Button button1;
 
