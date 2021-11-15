aaa7bb732cd540359aaa61de45201435b403f1d2
Petr Fuka <petr.fuka@gmail.com>
2021-11-15 17:14:29 +0100

    DLL version with UI controls

---
+++ vegas-clip-spacing/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace vegas_clip_spacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Location = new System.Drawing.Point(21, 22);
+            this.numericUpDown1.Maximum = new decimal(new int[] {600, 0, 0, 0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(144, 26);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {8, 0, 0, 0});
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(185, 126);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+        #endregion
+    }
+}
---
+++ vegas-clip-spacing/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ScriptPortal.Vegas;
+
+namespace vegas_clip_spacing
+{
+    public partial class Form1 : Form
+    {
+        public double OffsetSeconds;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Submit();
+        }
+
+        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) Keys.Return)
+            {
+                Submit();
+            }
+        }
+
+        private void Submit()
+        {
+            OffsetSeconds = (double) numericUpDown1.Value;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            List<TrackEvent> selected = FindSelectedEvents(vegas.Project);
+
+            if (selected.Count > 0)
+            {
+                double offsetSeconds;
+                using (Form1 form1 = new Form1())
+                {
+                    DialogResult result = form1.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        offsetSeconds = form1.OffsetSeconds;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+                var offset = Timecode.FromSeconds(offsetSeconds);
+                
+                // sort by Start, just in case
+                selected.Sort((first, second) => first.Start.CompareTo(second.Start));
+
+                // first clip is also moved
+                Timecode lastEnd = selected[0].Start;
+                foreach (TrackEvent trackEvent in selected)
+                {
+                    // move clip start to the end of the previous one plus offset
+                    trackEvent.AdjustStartLength(lastEnd + offset, trackEvent.Length, true);
+                    lastEnd = trackEvent.End;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No events selected.");
+            }
+        }
+        
+        /// get all selected events
+        List<TrackEvent> FindSelectedEvents(Project project)
+        {
+            List<TrackEvent> selectedEvents = new List<TrackEvent>();
+            foreach (Track track in project.Tracks)
+            {
+                foreach (TrackEvent trackEvent in track.Events)
+                {
+                    if (trackEvent.Selected)
+                    {
+                        selectedEvents.Add(trackEvent);
+                    }
+                }
+            }
+            return selectedEvents;
+        }
+    }
+}
